7757aac1aa58b92220424bf329edd700b6f21a05
tkozemzak <timkozemzak@gmail.com>
2021-03-08 11:48:20 -0700

    init

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const PORT = 4000;
+const app = express();
+
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+io.on("connection", (socket) => {
+  console.log("socket connected");
+  socket.on("disconnect", () => {
+    console.log("socket disconnected");
+  });
+  socket.on("join", (name) => {
+    console.log(name, "joined");
+    socket.name = name;
+    io.emit("join", name);
+  });
+  socket.on("chat", (data) => {
+    console.log(`chat data: ${JSON.stringify(data, null, 2)}`);
+    io.emit("chat", data);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log("Listening on: ", PORT);
+});
